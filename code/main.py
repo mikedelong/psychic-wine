9da0bf93efbc073ebd069b731c7f3e05bb549f92
mikedelong <mikedelong@outlook.com>
2018-06-26 14:11:28 -0400

    feature: add column names harvested from data source webpage

--- code/main.py
+++ code/main.py
@@ -16,10 +16,28 @@ if __name__ == '__main__':
     console_handler.setLevel(logging.DEBUG)
     logger.debug('started')
 
+    # 1) Alcohol
+    # 2) Malic acid
+    # 3) Ash
+    # 4) Alcalinity of ash
+    # 5) Magnesium
+    # 6) Total phenols
+    # 7) Flavanoids
+    # 8) Nonflavanoid phenols
+    # 9) Proanthocyanins
+    # 10)Color intensity
+    # 11)Hue
+    # 12)OD280 / OD315 of diluted wines
+    # 13)Proline
+
+    names = ['alcohol', 'malic_acid', 'ash', 'alcalinity_of_ash', 'magnesium', 'total_phenols', 'flavanoid_phenols',
+             'nonflavanoid_phenols', 'proanthcynins', 'color_intensity', 'hue', 'od_ratio', 'proline']
+
     input_file = '../data/wine.data'
     logger.debug('reading data from %s' % input_file)
-    data = pd.read_csv(input_file)
+    data = pd.read_csv(input_file, names=names)
     logger.debug(data.shape)
+    logger.debug(data.columns.values)
 
     logger.debug('done')
     finish_time = time.time()
