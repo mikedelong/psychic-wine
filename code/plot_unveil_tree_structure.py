4eae20caf18a5886ea9b523fced9c27f7494cf2b
mikedelong <mikedelong@outlook.com>
2018-06-29 09:14:31 -0400

    feature: introduce logging and timing

--- code/plot_unveil_tree_structure.py
+++ code/plot_unveil_tree_structure.py
@@ -16,118 +16,126 @@ show how to retrieve:
 - the decision path shared by a group of samples.
 
 """
+import logging
+import time
+
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
-iris = load_iris()
-X = iris.data
-y = iris.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+if __name__ == '__main__':
+    start_time = time.time()
 
-estimator = DecisionTreeClassifier(max_leaf_nodes=3, random_state=0)
-estimator.fit(X_train, y_train)
+    formatter = logging.Formatter('%(asctime)s : %(name)s :: %(levelname)s : %(message)s')
+    logger = logging.getLogger('main')
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+    console_handler.setLevel(logging.DEBUG)
+    logger.debug('started')
 
-# The decision estimator has an attribute called tree_  which stores the entire
-# tree structure and allows access to low level attributes. The binary tree
-# tree_ is represented as a number of parallel arrays. The i-th element of each
-# array holds information about the node `i`. Node 0 is the tree's root. NOTE:
-# Some of the arrays only apply to either leaves or split nodes, resp. In this
-# case the values of nodes of the other type are arbitrary!
-#
-# Among those arrays, we have:
-#   - left_child, id of the left child of the node
-#   - right_child, id of the right child of the node
-#   - feature, feature used for splitting the node
-#   - threshold, threshold value at the node
-#
+    iris = load_iris()
+    X = iris.data
+    y = iris.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
 
-# Using those arrays, we can parse the tree structure:
+    estimator = DecisionTreeClassifier(max_leaf_nodes=3, random_state=0)
+    estimator.fit(X_train, y_train)
 
-n_nodes = estimator.tree_.node_count
-children_left = estimator.tree_.children_left
-children_right = estimator.tree_.children_right
-feature = estimator.tree_.feature
-threshold = estimator.tree_.threshold
+    # The decision estimator has an attribute called tree_  which stores the entire
+    # tree structure and allows access to low level attributes. The binary tree
+    # tree_ is represented as a number of parallel arrays. The i-th element of each
+    # array holds information about the node `i`. Node 0 is the tree's root. NOTE:
+    # Some of the arrays only apply to either leaves or split nodes, resp. In this
+    # case the values of nodes of the other type are arbitrary!
+    #
+    # Among those arrays, we have:
+    #   - left_child, id of the left child of the node
+    #   - right_child, id of the right child of the node
+    #   - feature, feature used for splitting the node
+    #   - threshold, threshold value at the node
+    #
 
-# The tree structure can be traversed to compute various properties such
-# as the depth of each node and whether or not it is a leaf.
-node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
-is_leaves = np.zeros(shape=n_nodes, dtype=bool)
-stack = [(0, -1)]  # seed is the root node id and its parent depth
-while len(stack) > 0:
-    node_id, parent_depth = stack.pop()
-    node_depth[node_id] = parent_depth + 1
+    # Using those arrays, we can parse the tree structure:
 
-    # If we have a test node
-    if (children_left[node_id] != children_right[node_id]):
-        stack.append((children_left[node_id], parent_depth + 1))
-        stack.append((children_right[node_id], parent_depth + 1))
-    else:
-        is_leaves[node_id] = True
+    n_nodes = estimator.tree_.node_count
+    children_left = estimator.tree_.children_left
+    children_right = estimator.tree_.children_right
+    feature = estimator.tree_.feature
+    threshold = estimator.tree_.threshold
 
-print("The binary tree structure has %s nodes and has "
-      "the following tree structure:"
-      % n_nodes)
-for i in range(n_nodes):
-    if is_leaves[i]:
-        print("%snode=%s leaf node." % (node_depth[i] * "\t", i))
-    else:
-        print("%snode=%s test node: go to node %s if X[:, %s] <= %s else to "
-              "node %s."
-              % (node_depth[i] * "\t",
-                 i,
-                 children_left[i],
-                 feature[i],
-                 threshold[i],
-                 children_right[i],
-                 ))
-print()
+    # The tree structure can be traversed to compute various properties such
+    # as the depth of each node and whether or not it is a leaf.
+    node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
+    is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+    stack = [(0, -1)]  # seed is the root node id and its parent depth
+    while len(stack) > 0:
+        node_id, parent_depth = stack.pop()
+        node_depth[node_id] = parent_depth + 1
 
-# First let's retrieve the decision path of each sample. The decision_path
-# method allows to retrieve the node indicator functions. A non zero element of
-# indicator matrix at the position (i, j) indicates that the sample i goes
-# through the node j.
+        # If we have a test node
+        if (children_left[node_id] != children_right[node_id]):
+            stack.append((children_left[node_id], parent_depth + 1))
+            stack.append((children_right[node_id], parent_depth + 1))
+        else:
+            is_leaves[node_id] = True
 
-node_indicator = estimator.decision_path(X_test)
+    logger.debug("The binary tree structure has %s nodes and has the following tree structure:" % n_nodes)
+    for i in range(n_nodes):
+        if is_leaves[i]:
+            logger.debug("%snode=%s leaf node." % (node_depth[i] * "\t", i))
+        else:
+            logger.debug("%snode=%s test node: go to node %s if X[:, %s] <= %s else to node %s." % (
+                node_depth[i] * "\t", i, children_left[i], feature[i], threshold[i], children_right[i]))
 
-# Similarly, we can also have the leaves ids reached by each sample.
+    # First let's retrieve the decision path of each sample. The decision_path
+    # method allows to retrieve the node indicator functions. A non zero element of
+    # indicator matrix at the position (i, j) indicates that the sample i goes
+    # through the node j.
 
-leave_id = estimator.apply(X_test)
+    node_indicator = estimator.decision_path(X_test)
 
-# Now, it's possible to get the tests that were used to predict a sample or
-# a group of samples. First, let's make it for the sample.
+    # Similarly, we can also have the leaves ids reached by each sample.
 
-sample_id = 0
-node_index = node_indicator.indices[node_indicator.indptr[sample_id]:
-                                    node_indicator.indptr[sample_id + 1]]
+    leave_id = estimator.apply(X_test)
 
-print('Rules used to predict sample %s: ' % sample_id)
-for node_id in node_index:
-    if leave_id[sample_id] != node_id:
-        continue
+    # Now, it's possible to get the tests that were used to predict a sample or
+    # a group of samples. First, let's make it for the sample.
 
-    if (X_test[sample_id, feature[node_id]] <= threshold[node_id]):
-        threshold_sign = "<="
-    else:
-        threshold_sign = ">"
+    sample_id = 0
+    node_index = node_indicator.indices[node_indicator.indptr[sample_id]:
+                                        node_indicator.indptr[sample_id + 1]]
 
-    print("decision id node %s : (X_test[%s, %s] (= %s) %s %s)"
-          % (node_id,
-             sample_id,
-             feature[node_id],
-             X_test[sample_id, feature[node_id]],
-             threshold_sign,
-             threshold[node_id]))
+    logger.debug('Rules used to predict sample %s: ' % sample_id)
+    for node_id in node_index:
+        if leave_id[sample_id] != node_id:
+            continue
 
-# For a group of samples, we have the following common node.
-sample_ids = [0, 1]
-common_nodes = (node_indicator.toarray()[sample_ids].sum(axis=0) ==
-                len(sample_ids))
+        if (X_test[sample_id, feature[node_id]] <= threshold[node_id]):
+            threshold_sign = "<="
+        else:
+            threshold_sign = ">"
 
-common_node_id = np.arange(n_nodes)[common_nodes]
+        logger.debug("decision id node %s : (X_test[%s, %s] (= %s) %s %s)" % (
+            node_id, sample_id, feature[node_id], X_test[sample_id, feature[node_id]], threshold_sign,
+            threshold[node_id]))
 
-print("\nThe following samples %s share the node %s in the tree"
-      % (sample_ids, common_node_id))
-print("It is %s %% of all nodes." % (100 * len(common_node_id) / n_nodes,))
+    # For a group of samples, we have the following common node.
+    sample_ids = [0, 1]
+    common_nodes = (node_indicator.toarray()[sample_ids].sum(axis=0) == len(sample_ids))
+
+    common_node_id = np.arange(n_nodes)[common_nodes]
+
+    logger.debug("The following samples %s share the node %s in the tree"
+                 % (sample_ids, common_node_id))
+    logger.debug("It is %s %% of all nodes." % (100 * len(common_node_id) / n_nodes,))
+
+    logger.debug('done')
+    finish_time = time.time()
+    elapsed_hours, elapsed_remainder = divmod(finish_time - start_time, 3600)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_remainder, 60)
+    logger.info('Time: {:0>2}:{:0>2}:{:05.2f}'.format(int(elapsed_hours), int(elapsed_minutes), elapsed_seconds))
+    console_handler.close()
+    logger.removeHandler(console_handler)
